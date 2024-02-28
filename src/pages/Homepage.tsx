d686390b1b221534d1bb7da2e992cc7a996ea776
Gaetano <gaetano.alessandretto@gmail.com>
2024-02-28 22:14:46 +0100

    feat(master):delete axus and insert call fetch

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
-export default function Homepage() {
-    const [data, setData] = useState<any>({ hits: [] });
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios(
-                'api/fetch',
-            );
-            console.log(result.data)
-            setData(result.data);
-        };
 
-        fetchData();
-    }, []);
+export default function Homepage() {
+    const [products, setData] = useState<any>([]);
+
+    const fetchProducts = async () => {
+        try {
+            const res = await fetch('api/fetch');
+            const data: any = await res.json();
+            setData(data);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
     return (
         <ul>
-            {data.map((item: any) => (
-                <li key={item.objectID}>
-                    <h1>{item.title}</h1>
+            {Array.isArray(products) ? (
+                products.map((item: any) => (
+                    <li key={item.objectID}>
+                        <h1>{item.title}</h1>
+                    </li>
+                ))
+            ) : (
+                <li>
+                    <p>Dati non validi o vuoti.</p>
                 </li>
-            ))}
+            )}
         </ul>
     );
 }
-
--- src/pages/api/fetch.ts
+++ src/pages/api/fetch.ts
@@ -1,11 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function fetchData(request: NextApiRequest, response: NextApiResponse<any>) {
-    try {
-        const apiResponse = await fetch("https://mockend.up.railway.app/api/products");
-        const data = await apiResponse.json();
-        response.status(200).json(data);
-    } catch (error) {
-        console.error("Errore durante il recupero dei dati:", error);
-    }
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    const response = await fetch('https://mockend.up.railway.app/api/products');
+    const products = await response.json();
+
+    res.status(200).json(products);
 }
